f53b682df6fb8bbec783aed71bd872682e434f8d
Kevin Wong <ckevin2018@aliyun.com>
2025-10-25 14:00:17 +0800

    docs(snippets): 新增代码片段首页

--- dsa/_meta.ts
+++ dsa/_meta.ts
@@ -1,5 +1,5 @@
 export default {
-  'index': '数据结构与算法简介',
+  index: '数据结构与算法简介',
   'hash-table': '哈希表',
   'avl-tree': 'AVL 树',
   heap: '堆',
--- snippets/_meta.ts
+++ snippets/_meta.ts
@@ -1,3 +1,4 @@
 export default {
+    index: '代码片段简介',
     crc32: 'CRC32 算法',
 }
